afb257796d208c2a175404294a09df581e1f423d
Mammbo <113815614+Mammbo@users.noreply.github.com>
2024-06-24 17:12:24 -0500

    finsihed the ui now doing logic with js

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -2,57 +2,23 @@
 // paper beats rock and loses to scissors
 // scissors beats paper and loses to rock 
 
-function getComputerChoice() {
-    const MAX_CHOICES = 3
-    let computerChoice = Math.floor(Math.random() * MAX_CHOICES);
 
-    if (computerChoice === 0) {
-        computerChoice = "rock"
-    } else if (computerChoice === 1) {
-        computerChoice = "paper" 
-    } else {
-        computerChoice = "scissors"
-    }
-    return computerChoice
+
+/* .modal.active {
+  transform: translate(-50%, -50%) scale(1);
 }
 
-function getHumanChoice() { 
-    let choice = prompt("Rock, Paper, Scissors?)")
-    let lowered_choice = choice.toLowerCase()
-    return lowered_choice
+.overlay.active {
+  display: block;
 }
 
-function playRound() {
-    for (let turn = 0; turn < 5; turn++) {
+display both those values at the end in css and add active to both classes and disable display in overlay and transform in modal. */
 
-        const humanChoice = getHumanChoice();
-        const computerChoice = getComputerChoice();
-        if (computerChoice === humanChoice) {
-            console.log("Tie!, no one wins!");
-        } else if ((humanChoice === "paper" && computerChoice === "rock") || (humanChoice === "rock" && computerChoice === "scissors") || (humanChoice === "scissors" && computerChoice === "paper") ) { 
-            console.log("You win! " + humanChoice + " beats " + computerChoice)
-            human_score = human_score + 1
-        } else {
-            console.log("You lose :< " + computerChoice + " beats " + humanChoice)
-            computer_score = computer_score + 1
-        }
-    }   
-}
 
-function playGame(humanChoice, computerChoice) {
-        playRound(humanChoice, computerChoice)
-}
+// okay so the logic goes as follow 
+// 1. blank screeen telling the user to pick a weapon 
 
-let human_score = 0
-let computer_score = 0 
-
-playGame();
-
-if (human_score > computer_score) {
-    console.log("YOU WINN THE GAMEEE WOOOOOOO!")
-} else if (computer_score > human_score) { 
-    console.log("you lost. .-. :<")
-} else {
-    console.log("honestly more impressive, its really hard to tie but you did it good job! :D")
-}
-
+// once their choice is clicked which needs to be listend to by a click enviornment lister, the computer program will run and give the random choice.
+// the choose weapon next needs to be changed via js based off of the result of the match (tie win loss) and the text below that needs to display the choices ex: paper(human) beats rock(computer)!
+//2. while that happens a point needs to be added to the player score. 
+//3. once either player or computer reaches 5 the game is over and a pop up will appear asking to play again at that point new css elements will need to be added and changed along with the html file so the modal can popup. once clicked the dom will be reset restarting the program. 
